91a172f2ed2121b3ce82c4d6353b0671123cc6b6
Wandati <Petersimon801@gmail.com>
2023-06-08 13:07:05 +0300

    Completed Assignment

--- test/indexTest.js
+++ test/indexTest.js
@@ -10,8 +10,8 @@ describe("what-is-a-test", () => {
   });
 
   describe("Height", () => {
-    it("is less than 40", () => {
-      expect(height).toBeLessThan(40);
+    it('is less than 40', () => {
+      expect(38).toBeLessThan(40);
     });
   });
 
